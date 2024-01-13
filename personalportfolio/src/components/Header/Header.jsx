ce6463eaa593dc4ef9b95b931b31d943bbe60233
Yash Raj <yashraj941015@gmail.com>
2024-01-13 20:45:44 +0530

    check cuurent page isAvtive

--- personalportfolio/src/components/Header/Header.jsx
+++ personalportfolio/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom';
 
-// the Link is the replacement of the a tag becuase a tag makes a page refreshed when clicked 
+// the Link is the replacement of the "a" tag becuase 'a' tag makes a page refreshed when clicked 
 
 export default function Header() {
     return (
@@ -36,8 +36,9 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    className={() =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    className={(isActive) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0
+                                        ${isActive ? "text-orange-700" :"text-gray-700"}`
                                     }
                                 >
                                     Home
