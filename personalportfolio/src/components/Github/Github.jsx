b330634730a8420461a0a9352cc2f0c71c4f85fa
Yash Raj <yashraj941015@gmail.com>
2024-01-14 15:38:27 +0530

    uodated github compoent

--- personalportfolio/src/components/Github/Github.jsx
+++ personalportfolio/src/components/Github/Github.jsx
@@ -3,6 +3,7 @@ import { useLoaderData } from 'react-router-dom'
 
 function Github() {
     const data = useLoaderData()
+    console.log(data)
     // const [data, setData] = useState([])
     // useEffect(() => {
     //  fetch('https://api.github.com/users/hiteshchoudhary')
@@ -14,14 +15,17 @@ function Github() {
     // }, [])
     
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
-    <img src={data.avatar_url} alt="Git picture" width={300} />
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-2xl flex flex-row gap-5'>Github followers: {data.followers}
+    <img className='w-20' src={data.avatar_url} alt="Git picture" width={300} />
+    <h5>Company: {data.company}</h5>
+    <h5>Tech: {data.bio}</h5>
     </div>
   )
 }
 
 export default Github
 
+// it works as a promise
 export const githubInfoLoader = async () => {
     const response = await fetch('https://api.github.com/users/yashraj9410')
     return response.json()
