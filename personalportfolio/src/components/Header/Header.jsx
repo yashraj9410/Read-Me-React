697dfbf70f81b97c5fb295c5e8819c089d5442e7
Yash Raj <yashraj941015@gmail.com>
2024-01-14 16:21:29 +0530

    updated about component

--- personalportfolio/src/components/Header/Header.jsx
+++ personalportfolio/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link, NavLink} from 'react-router-dom'
+import logo from '../../assets/Screenshot 2024-01-14 161217.png'
 
 export default function Header() {
     return (
@@ -8,7 +9,7 @@ export default function Header() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                            src={logo}
                             className="mr-3 h-12"
                             alt="Logo"
                         />
